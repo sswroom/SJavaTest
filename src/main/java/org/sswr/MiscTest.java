9323eddd59fb7ec446794574f41b1d71891a5f26
Simon Wong <sswroom@yahoo.com>
2021-05-17 18:29:55 +0800

    Added test for fixDouble and fromDouble

--- src/main/java/org/sswr/MiscTest.java
+++ src/main/java/org/sswr/MiscTest.java
@@ -1,9 +1,17 @@
 package org.sswr;
 
+import org.sswr.util.data.StringUtil;
+
 public class MiscTest
 {
 	public static void main(String args[])
 	{
-		System.out.println(MiscTest.class.getPackage().getName());
+		double v1 = Double.parseDouble("5.0");
+		double v2 = Double.parseDouble("5.02");
+		System.out.println(StringUtil.fromDouble((v2 - v1) * 1000));
+		double v3 = 5.0;
+		double v4 = 5.02;
+		System.out.println(StringUtil.fixDouble(v4 - v3));
+		System.out.println(StringUtil.fixDouble((v4 - v3) * 1000.0));
 	}
 }
