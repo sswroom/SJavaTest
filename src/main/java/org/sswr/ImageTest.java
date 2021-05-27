455d2cccfac1f584a631b072a4e0879d0dafda0a
Simon Wong <sswroom@yahoo.com>
2021-05-27 17:31:06 +0800

    Added Resizer Test

--- src/main/java/org/sswr/ImageTest.java
+++ src/main/java/org/sswr/ImageTest.java
@@ -4,8 +4,16 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 
+import javax.imageio.ImageIO;
+
+import org.sswr.util.data.DataTools;
 import org.sswr.util.media.ImageList;
 import org.sswr.util.media.ImageUtil;
+import org.sswr.util.media.NearestNeighbourResizer;
+import org.sswr.util.media.ResizeAspectRatio;
+import org.sswr.util.media.Resizer;
+import org.sswr.util.media.Size2D;
+import org.sswr.util.media.StaticImage;
 
 public class ImageTest {
 	public static void main(String args[])
@@ -17,7 +25,19 @@ public class ImageTest {
 			{
 				System.out.println("Error in reading image");
 			}
-			if (ImageUtil.saveAsJpg(imgList.getImage(0), new FileOutputStream("/home/sswroom/Progs/photo2.jpg"), 1.0f))
+			StaticImage img = imgList.getImage(0);
+			Size2D size = Resizer.calcOutputSize(img.getWidth(), img.getHeight(), img.getPixelAspectRatio(), 200, 200, ResizeAspectRatio.SQUARE_PIXEL);
+			StaticImage newImg = new NearestNeighbourResizer().resize(img, size);
+			if (ImageUtil.saveAsJpg(newImg, new FileOutputStream("/home/sswroom/Progs/photo2.jpg"), 1.0f))
+			{
+				System.out.println("Small Image exported");
+			}
+			else
+			{
+				System.out.println("Error in exporting to jpg");
+			}
+			/*
+			if (ImageUtil.saveAsJpg(img, new FileOutputStream("/home/sswroom/Progs/photo2.jpg"), 1.0f))
 			{
 				System.out.println("Image exported");
 			}
@@ -25,14 +45,14 @@ public class ImageTest {
 			{
 				System.out.println("Error in exporting to jpg");
 			}
-			if (ImageUtil.saveAsJpgBySize(imgList.getImage(0), new FileOutputStream("/home/sswroom/Progs/photo3.jpg"), 1500000, 2000000))
+			if (ImageUtil.saveAsJpgBySize(img, new FileOutputStream("/home/sswroom/Progs/photo3.jpg"), 1500000, 2000000))
 			{
 				System.out.println("Image by size exported");
 			}
 			else
 			{
 				System.out.println("Error in exporting to jpg by size");
-			}
+			}*/
 		}
 		catch (FileNotFoundException ex)
 		{
