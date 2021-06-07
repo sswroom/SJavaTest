3ce43c22f4335bd34e1218c17eed90ba42e84aff
Simon Wong <sswroom@yahoo.com>
2021-06-07 20:42:06 +0800

    Added JasyptEncryptor Testing

--- src/main/java/org/sswr/PBEWITHHMACSHA512ANDAES_256Test.java
+++ src/main/java/org/sswr/PBEWITHHMACSHA512ANDAES_256Test.java
@@ -1,18 +1,21 @@
 package org.sswr;
 
-import org.sswr.util.data.StringUtil;
+import org.sswr.util.crypto.JasyptEncryptor;
+import org.sswr.util.crypto.JasyptEncryptor.CipherAlgorithm;
+import org.sswr.util.crypto.JasyptEncryptor.KeyAlgorithm;
 
 public class PBEWITHHMACSHA512ANDAES_256Test
 {
 	public static void main(String args[])
 	{
+		String encPwd = "test";
+		String userName = "testing";
+		String password = "testing";
 		try
 		{
-			PBEWITHHMACSHA512ANDAES_256 enc = new PBEWITHHMACSHA512ANDAES_256("testing");
-			String userName = "abcd";
-			String password = "abcd";
-			System.out.println("Username = " + StringUtil.toHex(enc.decrypt(userName)));
-			System.out.println("Password = " + StringUtil.toHex(enc.decrypt(password)));
+			JasyptEncryptor enc = new JasyptEncryptor(KeyAlgorithm.PBEWITHHMACSHA512, CipherAlgorithm.AES256, encPwd);
+			System.out.println("Username = " + enc.decryptToString(userName));
+			System.out.println("Password = " + enc.decryptToString(password));
 		}
 		catch (Exception ex)
 		{
