5596d456fc612dac089608dc1fca147bfcbe8b8e
Simon Wong <sswroom@yahoo.com>
2021-06-11 23:15:00 +0800

    Added SNMPMIB Test

--- src/main/java/org/sswr/SNMPTest.java
+++ src/main/java/org/sswr/SNMPTest.java
@@ -1,6 +1,15 @@
 package org.sswr;
 
+import org.sswr.util.net.SNMPMIB;
+
 public class SNMPTest
 {
-	
+	public static void main(String args[])
+	{
+		SNMPMIB mib = new SNMPMIB();
+		StringBuilder sb = new StringBuilder();
+		mib.loadFile("/home/sswroom/Progs/VCClass/MIBs/NAS-MIB", sb);
+		System.out.println(sb.toString());
+		System.out.println(mib.toString());
+	}
 }
