0567e9da1c809c010b67e937d6375541293a6e5b
Simon Wong <sswroom@yahoo.com>
2021-06-13 22:13:58 +0800

    Added SNMP Client Test

--- src/main/java/org/sswr/SNMPTest.java
+++ src/main/java/org/sswr/SNMPTest.java
@@ -1,15 +1,53 @@
 package org.sswr;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.sswr.util.net.SNMPBindingItem;
+import org.sswr.util.net.SNMPClient;
+import org.sswr.util.net.SNMPErrorStatus;
 import org.sswr.util.net.SNMPMIB;
 
 public class SNMPTest
 {
-	public static void main(String args[])
+	private static void snmpMIBTest()
 	{
 		SNMPMIB mib = new SNMPMIB();
 		StringBuilder sb = new StringBuilder();
 		mib.loadFile("/home/sswroom/Progs/VCClass/MIBs/NAS-MIB", sb);
 		System.out.println(sb.toString());
 		System.out.println(mib.toString());
+	}
+
+	private static void snmpWalkTest()
+	{
+		SNMPClient cli = new SNMPClient();
+		List<SNMPBindingItem> itemList = new ArrayList<SNMPBindingItem>();
+		SNMPErrorStatus err;
+		try
+		{
+			err = cli.v1Walk(InetAddress.getByName("192.168.0.110"), "public", "1.3.6.1.2.1", itemList);
+			System.out.println("Error = " + err);
+			int i = 0;
+			int j = itemList.size();
+			while (i < j)
+			{
+				System.out.println(itemList.get(i));
+				i++;
+			}
+		}
+		catch (UnknownHostException ex)
+		{
+			ex.printStackTrace();
+		}
+		cli.close();
+	}
+
+	public static void main(String args[])
+	{
+		snmpMIBTest();
+		snmpWalkTest();
 	}
 }
---
+++ src/main/java/org/sswr/UDPEchoTest.java
@@ -0,0 +1,45 @@
+package org.sswr;
+
+import java.io.IOException;
+import java.net.InetAddress;
+
+import org.sswr.util.io.LogTool;
+import org.sswr.util.net.UDPPacketListener;
+import org.sswr.util.net.UDPServer;
+
+public class UDPEchoTest implements UDPPacketListener
+{
+	private UDPServer svr;
+	private LogTool log;
+
+	public UDPEchoTest(int port)
+	{
+		this.log = new LogTool();
+		this.svr = new UDPServer(null, port, null, this, this.log, "UDP:", 3, false);
+	}
+
+	public void close()
+	{
+		this.svr.close();
+	}
+
+	@Override
+	public void udpPacketReceived(InetAddress addr, int port, byte[] buff, int ofst, int length) {
+		System.out.println("Received "+length+" bytes");
+		this.svr.sendTo(addr, port, buff, ofst, length);
+	}
+
+	public static void main(String args[])
+	{
+		UDPEchoTest test = new UDPEchoTest(3456);
+		try
+		{
+			System.in.read();
+		}
+		catch (IOException ex)
+		{
+			ex.printStackTrace();
+		}
+		test.close();
+	}
+}
