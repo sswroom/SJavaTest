dc22cc5dcba517a2fe8709b588ad41b16a402168
Simon Wong <sswroom@yahoo.com>
2021-06-23 00:45:03 +0800

    Added SNMP Manager Test

--- src/main/java/org/sswr/SNMPTest.java
+++ src/main/java/org/sswr/SNMPTest.java
@@ -5,10 +5,13 @@ import java.net.UnknownHostException;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.sswr.util.data.DataTools;
+import org.sswr.util.net.SNMPAgentInfo;
 import org.sswr.util.net.SNMPBindingItem;
 import org.sswr.util.net.SNMPClient;
 import org.sswr.util.net.SNMPErrorStatus;
 import org.sswr.util.net.SNMPMIB;
+import org.sswr.util.net.SNMPManager;
 
 public class SNMPTest
 {
@@ -45,9 +48,27 @@ public class SNMPTest
 		cli.close();
 	}
 
+	public static void snmpManagerTest()
+	{
+		SNMPManager mgr = new SNMPManager(null);
+		List<SNMPAgentInfo> agentList = new ArrayList<SNMPAgentInfo>();
+		try
+		{
+			InetAddress addr = InetAddress.getByName("192.168.0.255");
+			mgr.addAgents(addr, "public", agentList, true);
+			System.out.println("Agents: "+DataTools.toObjectString(agentList));
+		}
+		catch (UnknownHostException ex)
+		{
+			ex.printStackTrace();
+		}
+		mgr.close();
+	}
+
 	public static void main(String args[])
 	{
-		snmpMIBTest();
-		snmpWalkTest();
+		snmpManagerTest();
+//		snmpMIBTest();
+//		snmpWalkTest();
 	}
 }
