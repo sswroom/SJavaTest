6e6a890bef06845368bd51bfaa7ff5e5db598e04
Simon Wong <sswroom@yahoo.com>
2021-07-14 01:12:38 +0800

    Added OTP Test

--- src/main/java/org/sswr/MiscTest.java
+++ src/main/java/org/sswr/MiscTest.java
@@ -2,12 +2,22 @@ package org.sswr;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.ByteBuffer;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 import java.util.List;
+import java.util.TimeZone;
+
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
 
 import org.sswr.util.data.DataTools;
 import org.sswr.util.data.DateTimeUtil;
 import org.sswr.util.data.SharedLong;
 import org.sswr.util.data.StringUtil;
+import org.sswr.util.data.textbinenc.Base32Enc;
 import org.sswr.util.io.OSInfo;
 import org.sswr.util.io.ResourceLoader;
 import org.sswr.util.io.StreamUtil;
@@ -63,9 +73,57 @@ public class MiscTest
 		System.out.println("OS = "+OSInfo.getOSType());
 	}
 
+
+	public static byte[] otpDigest(byte[] secret, long currentInterval) throws NoSuchAlgorithmException, InvalidKeyException
+	{
+        byte[] challenge = ByteBuffer.allocate(8).putLong(currentInterval).array();
+        Mac mac = Mac.getInstance("HMACSHA1");
+        SecretKeySpec macKey = new SecretKeySpec(secret, "RAW");
+        mac.init(macKey);
+        return mac.doFinal(challenge);
+	}
+
+	public static int bytesToInt(byte[] hash)
+	{
+        // put selected bytes into result int
+        int offset = hash[hash.length - 1] & 0xf;
+
+        int binary = ((hash[offset] & 0x7f) << 24) |
+                ((hash[offset + 1] & 0xff) << 16) |
+                ((hash[offset + 2] & 0xff) << 8) |
+                (hash[offset + 3] & 0xff);
+
+        return binary % 1000000;
+    }
+
+	public static void otpTest()
+	{
+		String keyStr = "ZIBCVVORZW63LWLLLOT6LRAYSNSIH7AE";
+		Base32Enc b32 = new Base32Enc();
+		byte[] secret = b32.decodeBin(keyStr);
+		System.out.println("Secret = "+StringUtil.toHex(secret));
+
+		int interval = 30;
+		Calendar calendar = GregorianCalendar.getInstance(TimeZone.getTimeZone("UTC"));
+		long currentTimeSeconds = calendar.getTimeInMillis() / 1000;
+		long currentInterval = (currentTimeSeconds / interval);
+		System.out.println("Counter = " + currentInterval);
+
+		try
+		{
+			byte out[] = otpDigest(secret, currentInterval);
+			System.out.println("Hash = "+StringUtil.toHex(out));
+			System.out.println("Code = "+bytesToInt(out));
+		}
+		catch (Exception ex)
+		{
+			ex.printStackTrace();
+		}
+	}
+
 	public static void main(String args[])
 	{
-		int type = 1;
+		int type = 4;
 		switch (type)
 		{
 		case 0:
@@ -79,6 +137,9 @@ public class MiscTest
 			break;
 		case 3:
 			detectOS();
+			break;
+		case 4:
+			otpTest();
 			break;
 		}	
 	}
