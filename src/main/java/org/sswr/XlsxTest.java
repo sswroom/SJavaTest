051a0b23d244b275760f4025e8ce04e7d7a8e10f
Simon Wong <sswroom@yahoo.com>
2021-11-10 18:54:42 +0800

    Added xlsxTest

---
+++ src/main/java/org/sswr/XlsxTest.java
@@ -0,0 +1,22 @@
+package org.sswr;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import org.sswr.util.data.DataTools;
+import org.sswr.util.io.FileUtil;
+import org.sswr.util.office.XlsxValidator;
+
+public class XlsxTest
+{
+	public static void main(String []args) throws IOException
+	{
+		String fileName = FileUtil.getRealPath("~/Progs/Temp/Test.xlsx");
+		XlsxValidator xlsx = new XlsxValidator(new FileInputStream(fileName), new String[]{"ColA", "ColB", "ColC", "ColD"});
+		while (xlsx.nextRow())
+		{
+			System.out.println(DataTools.toObjectString(xlsx.getRowAsMap()));
+		}
+		xlsx.close();
+	}	
+}
