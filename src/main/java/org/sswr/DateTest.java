ca9a6063fe4d0829d965d81f7e149510f38ff81b
Simon Wong <sswroom@yahoo.com>
2021-12-28 17:03:21 +0800

    Enhance DateTime Testing

--- src/main/java/org/sswr/DateTest.java
+++ src/main/java/org/sswr/DateTest.java
@@ -15,5 +15,14 @@ public class DateTest
 		System.out.println("zdt.t = "+DateTimeUtil.getTimeMillis(zdt));
 		System.out.println("toTS(zdt) = "+DateTimeUtil.toTimestamp(zdt));
 		System.out.println(DateTimeUtil.parse("2020-12-31T16:00:00.123+00:00"));
+		System.out.println("Year start = "+DateTimeUtil.toYearStart(zdt));
+
+		int i = 1;
+		while (i <= 365)
+		{
+			ZonedDateTime dt = zdt.withDayOfYear(i);
+			System.out.println(dt+": "+((dt.getDayOfYear() - 1) / 7 + 1));
+			i++;
+		}
 	}
 }
