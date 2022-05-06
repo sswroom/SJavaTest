fe18a49666d06552ee43a4588e9bc1609692d1b2
Simon Wong <sswroom@yahoo.com>
2022-05-06 18:46:04 +0800

    Remove unused import

--- src/main/java/org/sswr/MiscTest.java
+++ src/main/java/org/sswr/MiscTest.java
@@ -5,7 +5,6 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.InetAddress;
-import java.net.Socket;
 import java.nio.ByteBuffer;
 import java.nio.charset.StandardCharsets;
 import java.security.InvalidKeyException;
